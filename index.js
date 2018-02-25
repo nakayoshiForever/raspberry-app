ec7d1f20afa12514217895aa87385c3673c4efa4
Your Name <you@example.com>
2018-02-25 10:37:08 +0900

    complete websock.

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const fs = require('fs')
 const socket = new WebSocket('ws://forever-hackchu.mybluemix.net/ws/example');
 
 socket.addEventListener('open', function (event) {
-	console.log('socket open....')
-	const audio = fs.createReadStream('./data/001-sibutomo.mp3')
-	socket.send(audio)
+    console.log('socket open....')
+    const audio = fs.createReadStream('./data/001-sibutomo.mp3')
+    audio.on('data', function(buffer) {
+        console.log(buffer)
+        socket.send(buffer)
+    })
 });
 
 socket.addEventListener('message', function (event) {
-	console.log('Message from server', event.data);
+    console.log('Message from server', event.data);
 });
