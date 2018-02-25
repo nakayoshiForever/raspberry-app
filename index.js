39586da77137b17f421c58e6b9771278b551c1c7
Your Name <you@example.com>
2018-02-25 10:30:23 +0900

    10:30

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 'use strict'
 
 console.log('read app....')
+
 const WebSocket = require('ws')
 const fs = require('fs')
 
-// WebSocket を作成
 const socket = new WebSocket('ws://forever-hackchu.mybluemix.net/ws/example');
 
-// 接続を開く
 socket.addEventListener('open', function (event) {
 	console.log('socket open....')
 	const audio = fs.createReadStream('./data/001-sibutomo.mp3')
 	socket.send(audio)
 });
 
-// メッセージを Listen する
 socket.addEventListener('message', function (event) {
 	console.log('Message from server', event.data);
 });
